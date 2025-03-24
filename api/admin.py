78565a3e404f023fc28328a7d77eb22b0e28a673
David-Roklem <roklem@mail.ru>
2025-03-24 10:41:12 +0300

    add admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Category, Task, User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["name", "telegram_id"]
+    search_fields = ["name", "telegram_id"]
+    list_per_page = 10
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["category", "status", "title", "description", "due_date", "user"]
+    search_fields = ["title"]
+    list_filter = ("category", "status", "user")
+    list_per_page = 10
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+    search_fields = ["name"]
+    list_per_page = 10
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(Category, CategoryAdmin)
