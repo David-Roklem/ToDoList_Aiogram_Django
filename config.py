4d721ba7002daa9022e4d161215809cbd622d636
David-Roklem <roklem@mail.ru>
2025-04-04 17:50:46 +0300

    add redis,timezone etc to config

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from functools import lru_cache
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from aiogram import Dispatcher, Bot
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 
 class Settings(BaseSettings):
@@ -15,17 +18,31 @@ class Settings(BaseSettings):
     POSTGRES_PORT: str
     POSTGRES_HOST: str
 
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
+    # REDIS_PASSWORD: str = Field(...)
+
+    CELERY_MAIN: str
+
     BACKEND_SERVER_ADDRESS: str
 
+    TIMEZONE: str
+
     DB_URL: str = Field('', validation_alias='DB_URL')
+    REDIS_URL: str = Field('', validation_alias='REDIS_URL')
 
     def __init__(self, **data):
         super().__init__(**data)
         self.DB_URL = self.__construct_db_url()
+        self.REDIS_URL = self.__construct_redis_url()
 
     def __construct_db_url(self) -> str:
         return (f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@"
                 f"{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}")
+
+    def __construct_redis_url(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
 
 
 @lru_cache
@@ -35,3 +52,6 @@ def get_settings() -> Settings:
 
 
 settings = get_settings()
+
+bot = Bot(settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
